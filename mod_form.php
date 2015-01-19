605f4abffc6c4cdb1816854143692bf2fa885e58
Charles Verge <charles.verge@remote-learner.net>
2015-01-19 11:37:38 -0400

    KRONOSDEV-25: Activity settings autocomplete.

--- mod_form.php
+++ mod_form.php
@@ -38,7 +38,7 @@ class mod_kronossandvm_mod_form extends moodleform_mod {
      * Define form for settings.
      */
     public function definition() {
-        global $DB, $USER, $CFG;
+        global $DB, $USER, $CFG, $PAGE;
 
         $mform =& $this->_form;
         $options = $DB->get_records_menu("vm_courses", array('isactive' => 1), 'imageid', 'id, coursename');
@@ -46,6 +46,19 @@ class mod_kronossandvm_mod_form extends moodleform_mod {
         $canconfig = kronossandvm_canconfig();
 
         if (!empty($options)) {
+            // Virtual machine templates autocomplete.
+            $mform->addElement('hidden', 'otcourseid', '0', array('id' => 'id_config_vmcourses'));
+            $mform->setType('otcourseid', PARAM_INT);
+            $url = new moodle_url('/mod/kronossandvm/ajax.php');
+            $acdivid = 'ac_input';
+            $params = array(
+                'datasource' => $url->out(),
+                'divid' => $acdivid,
+                'course' => $this->current->course
+            );
+            $PAGE->requires->css('/mod/kronossandvm/styles.css');
+            $PAGE->requires->yui_module('moodle-mod_kronossandvm-vmcoursesautocmp', 'M.mod_kronossandvm.init', array($params), null, true);
+
             $mform->addElement('text', 'name', get_string('name'), array('size' => '64'));
             $mform->setType('name', PARAM_TEXT);
             $mform->addRule('name', null, 'required', null, 'client');
@@ -58,8 +71,26 @@ class mod_kronossandvm_mod_form extends moodleform_mod {
             $mform->setType('duration', PARAM_INT);
             $mform->addRule('duration', null, 'required', null, 'client');
 
-            $mform->addElement('select', 'otcourseid', get_string('otcourseid', 'kronossandvm'), $options);
-            $mform->addRule('otcourseid', get_string('required'), 'required', null, 'client');
+            $text = '';
+            if (isset($this->current->otcourseid) && is_numeric($this->current->otcourseid)) {
+                $record = $DB->get_record('vm_courses', array('id' => $this->current->otcourseid));
+                if (!empty($record)) {
+                    $text = format_string($record->coursename);
+                }
+            }
+
+            $inputoptions = array(
+                'id' => $acdivid,
+                'type' => 'text',
+                'size' => 51,
+                'maxlength' => '50',
+                'placeholder' => get_string('placeholder', 'mod_kronossandvm'),
+                'value' => $text
+            );
+            $mform->addElement('text', 'otcourseidsearch', get_string('otcourseid', 'kronossandvm'), $inputoptions);
+            $mform->setType('otcourseidsearch', PARAM_TEXT);
+            $mform->addRule('otcourseidsearch', get_string('required'), 'required', null, 'client');
+            $mform->addHelpButton('otcourseidsearch', 'otcourseidsearch', 'mod_kronossandvm');
 
             $this->add_action_buttons();
         }
